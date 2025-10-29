f84749dc6804dd582bf35c3eb9acbcee925afaab
arlindademaj <aarlindademaj@gmail.com>
2025-10-29 05:54:45 -1100

    delete products from HTML when clicked "delete"

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -16,8 +16,9 @@ cart.forEach((cartItem) => {
   });
 
   cartSummaryHTML += `
-  <div class="cart-item-container">
-   <div class="delivery-date">Delivery date: Tuesday, June 21</div>
+  <div class="cart-item-container 
+    js-cart-item-container-${matchingProduct.id}">
+    <div class="delivery-date">Delivery date: Tuesday, June 21</div>
 
   <div class="cart-item-details-grid">
     <img
@@ -94,5 +95,10 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
   link.addEventListener("click", () => {
     const productId = link.dataset.productId;
     removeFromCart(productId);
+
+    const conatiner = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    conatiner.remove();
   });
 });
