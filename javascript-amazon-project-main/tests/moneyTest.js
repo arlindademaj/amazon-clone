fa499161c7584d2857d911baf7568a0d6ff657ea
arlindademaj <aarlindademaj@gmail.com>
2025-10-31 09:43:40 -1100

    Create automated tests for formatCurrency

---
+++ javascript-amazon-project-main/tests/moneyTest.js
@@ -0,0 +1,35 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+console.log("Test Suite: formatCurrency");
+
+console.log("Converts cents into dollars!");
+
+if (formatCurrency(2095) === "20.95") {
+  console.log("passed, true");
+} else {
+  console.log("failed");
+}
+
+console.log("Works with 0.");
+
+if (formatCurrency(0) === "0.00") {
+  console.log("passed");
+} else {
+  console.log("failed");
+}
+
+console.log("Rounds up to the nearest cent!");
+
+if (formatCurrency(2000.5) === "20.01") {
+  console.log("passed");
+} else {
+  console.log("failed");
+}
+
+console.log("Rounds down to the neares cent");
+
+if (formatCurrency(2000.4) === "20.00") {
+  console.log("passed");
+} else {
+  console.log("failed");
+}
